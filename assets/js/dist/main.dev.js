9de15c27f43c88a889f2d00a4bdf62ea251c00f0
Nata <merlincat.no@gmail.com>
2021-06-08 12:22:50 +0300

    map and get_in_touch

--- assets/js/dist/main.dev.js
+++ assets/js/dist/main.dev.js
@@ -69,3 +69,24 @@ $(function () {
     }
   });
 });
+var map = L.map('map').setView([40.64740434388478, -73.9088304023876], 15);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+var myIcon = L.icon({
+  iconUrl: 'assets/js/plugins/leaflet/images/pin.png',
+  // shadowUrl: 'pin.png',
+  iconSize: [106, 106],
+  // size of the icon
+  // shadowSize:   [50, 64], // size of the shadow
+  // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  // shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+});
+L.marker([40.64740434388478, -73.9088304023876], {
+  icon: myIcon
+}).addTo(map).bindPopup('POP UP'); // Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+//     maxZoom: 20,
+//     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+// });
